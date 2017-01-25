a55f9f5213159c747ceda21ce2d827866bf2d7bd
Rodrigo Miguel Vieira <sdmiguelvieira@gmail.com>
2017-01-25 11:06:22 -0200

    add create-user

---
+++ gen-csr.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#Maintener Rodrigo Miguel rodrigomvi@algartech.com 
+#Required
+domain=$1
+commonname=$domain
+ 
+#Change to your company details
+country='BR'
+state='DF'
+locality='Brasilia'
+organization='Policia Federal'
+organizationalunit='Coordenacao-Geral de Tecnologia da Informacao - CGTI'
+email='sso.sst.cti@dpf.gov.br'
+ 
+#Optional
+password=ApToJB15951
+ 
+if [ -z "$domain" ]
+then
+    echo "Argument not present."
+    echo "Useage $0 [common name]"
+ 
+    exit 99
+fi
+ 
+echo "Generating key and Creating CSR request for $domain"
+ 
+ 
+#Create the request
+echo "Creating CSR"
+openssl req -newkey rsa:2048 -keyout $domain.key -keyform PEM -SHA256 -out $domain.csr -passout pass:$password \
+    -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+
+#Remove passphrase from the key. Comment the line out to keep the passphrase
+echo "Removing passphrase from key"
+openssl rsa -in $domain.key -passin pass:$password -out $domain.key
+
+
+	
+echo "---------------------------"
+echo "-----Below is your CSR-----"
+echo "---------------------------"
+echo
+cat $domain.csr
+ 
+echo
+echo "---------------------------"
+echo "-----Below is your Key-----"
+echo "---------------------------"
+echo
+cat $domain.key
